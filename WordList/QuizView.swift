4432cde04d89c25c6e10b6a34cd75aed2d7cbb47
MahoHayashi <is0792vf@ed.ritsumei.ac.jp>
2025-07-18 21:59:12 +0900

    クイズの進行機能

--- WordList/QuizView.swift
+++ WordList/QuizView.swift
@@ -9,10 +9,15 @@ import SwiftUI
 import SwiftData
 
 struct QuizView: View {
+    @Environment(\.dismiss) var dismiss
     @Environment(\.modelContext) var context
     @Query var words: [Word]
     @State var index = 0
     @State var showAnswer = false
+    
+    var isLastQuiz: Bool{
+        index + 1 >= words.count
+    }
     
     var body: some View {
         VStack(spacing: 24){
@@ -40,6 +45,24 @@ struct QuizView: View {
                 .clipShape(.capsule)
                 
             }
+            Spacer()
+            
+            Button{
+                if isLastQuiz{
+                    dismiss()
+                } else {
+                    index += 1
+                    showAnswer = false
+                }
+            }label:{
+                Text(isLastQuiz ? "終了!" : "次の問題へ")
+            }
+            .font(.title)
+            .foregroundStyle(.white)
+            .padding(.vertical, 12)
+            .frame(maxWidth: .infinity)
+            .background(Color.orange)
+            .clipShape(.capsule)
             
             Spacer()
         }
