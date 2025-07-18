2ad2add4634eac1fe31693d3c2e03cac1c042b76
MahoHayashi <is0792vf@ed.ritsumei.ac.jp>
2025-07-18 21:17:32 +0900

    Initial Commit

---
+++ WordList/WordListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordListApp.swift
+//  WordList
+//
+//  Created by maho hayashi on 2025/07/18.
+//
+
+import SwiftUI
+
+@main
+struct WordListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordListTests/WordListTests.swift
@@ -0,0 +1,16 @@
+//
+//  WordListTests.swift
+//  WordListTests
+//
+//  Created by maho hayashi on 2025/07/18.
+//
+
+import Testing
+
+struct WordListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
